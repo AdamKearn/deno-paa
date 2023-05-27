6b7781ec10978aa81f1434ea042354e7e3f45e78
Adam Kearn <adamkearn17@gmail.com>
2023-05-27 22:36:24 +0100

    renaming to args

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { format, parse } from "https://deno.land/std@0.189.0/path/mod.ts";
 
-import inputArgs from "./handlers/cli/commands.ts";
+import inputArgs from "./handlers/cli/args.ts";
 import { imageToPAA } from "./handlers/images/imageToPAA.ts";
 
 imageToPAA(
